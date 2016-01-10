1ab436bb97531a48ef76584d0614e1993117cc58
Praveen Gowda I V <praveen.vvstgy@gmail.com>
2016-01-10 18:32:09 +0530

    Initial Commit

---
+++ MenuCalendar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MenuCalendar
+//
+//  Created by Praveen Gowda I V on 1/10/16.
+//  Copyright © 2016 Praveen Gowda I V. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
