3fc6dbd8124203ab5700502ca012eacccba8d7ef
Praveen Gowda I V <praveen.vvstgy@gmail.com>
2016-01-10 20:37:40 +0530

    Initial Commit

--- MenuCalendar/AppDelegate.swift
+++ MenuCalendar/AppDelegate.swift
@@ -9,17 +9,53 @@
 import Cocoa
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, MLCalendarViewDelegate {
 
     @IBOutlet weak var window: NSWindow!
-
+    
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
+    
+    let popover = NSPopover()
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        
+        if let button = statusItem.button {
+            button.image = NSImage(named: "calendar")
+            button.action = Selector("toggleCalendar:")
+        }
+        
+        let calendar = MLCalendarView()
+        calendar.delegate = self
+        calendar.date  = NSDate()
+        
+        popover.contentViewController = calendar
+        popover.appearance = NSAppearance(named: NSAppearanceNameAqua)
+    }
+    
+    func toggleCalendar(sender: NSStatusBarButton) {
+        if popover.shown {
+            hideCalendar(sender)
+        } else {
+            showCalendar(sender)
+        }
+    }
+    
+    func showCalendar(sender: AnyObject) {
+        if let button = statusItem.button {
+            popover.showRelativeToRect(button.bounds, ofView: button, preferredEdge: .MinY)
+        }
+    }
+    
+    func hideCalendar(sender: AnyObject) {
+        popover.performClose(sender)
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
+    }
+    
+    func didSelectDate(selectedDate: NSDate!) {
+//        print(selectedDate)
     }
 
 
